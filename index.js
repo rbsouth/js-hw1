34a19f4ed707a9b0fa268ba394fbfcfc766d9fb0
Reid South <rbsouth@gmail.com>
2018-01-16 18:25:42 -0700

    q1 fixes #1

---
+++ index.js
@@ -0,0 +1,17 @@
+//1. Create a script with two variables, each assigned to a number. 
+//Add them together and output the result to the console. Now do the same with two strings (concatenation).
+/*
+var x = 2
+var y = 3
+
+console.log(x + y);
+
+var a = 'Reid'
+var b = 'South'
+
+console.log(a + b);
+*/
+
+//Create a multidimensional array. The array will be an array of Aston Martin cars, BMW cars, and Bentley cars,
+// 3 cars of each make.
+
